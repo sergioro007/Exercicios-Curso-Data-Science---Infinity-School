86fc2f47a790919eac1264251780f305aab59a62
Sérgio R Oliveira <111983762+sergioro007@users.noreply.github.com>
2022-08-24 10:42:25 -0300

    Add files via upload
    Neste exercício elaboramos um código que converte uma medida em metros para centímetros.

---
+++ sro_aula04_ex01.py
@@ -0,0 +1,14 @@
+# Exercício 01: Converter metros para centímetros
+
+print ("*********************************************")
+print ("           CONVERSOR DE MEDIDAS              ")
+print ("*********************************************")
+print ("")
+
+medidaM = float(input("Digite a medida em Metros: "))
+print("")
+medidaCm = medidaM*100
+
+print (f'{medidaM} metros equivalem a {medidaCm} Centímetros')
+print ("")
+print ("Obrigado por utilizar o nosso conversor")
