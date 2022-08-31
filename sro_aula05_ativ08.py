ba0bd351897a9b2b50d9c4cf0bb34ebdc5184e6b
Sérgio R Oliveira <111983762+sergioro007@users.noreply.github.com>
2022-08-31 11:19:57 -0300

    Add files via upload
    Solicita um número ao usuário, em seguida digita a sequencia de números até o número digitado pelo usuário. 
    Nos comentários: para número positivo digitado
    No código: para número negativo digitado

---
+++ sro_aula05_ativ08.py
@@ -0,0 +1,20 @@
+
+#num = int(input("Digite um número positivo: "))
+#print("")
+
+#i = 0
+
+#while i <= num:
+#    print(f'{i}')
+#    i = i + 1
+
+
+num = int(input("Digite um número negativo: "))
+print("")
+
+i = 0
+
+while i >= num:
+    print(f'{i}')
+    i = i - 1
+
