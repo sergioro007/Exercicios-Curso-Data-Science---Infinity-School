7a0d877f7b80fadec3eb61d0cb5db58c103793b1
Sérgio R Oliveira <111983762+sergioro007@users.noreply.github.com>
2022-08-31 11:06:56 -0300

    Add files via upload
    Imprime a sequencia de números pares e, em seguida a sequencia de ímpares (até 100)

---
+++ sro_aula05_ativ07.py
@@ -0,0 +1,15 @@
+
+contpares= 0
+contimpares = 1
+
+
+print("PARES:")
+while contpares <= 100:
+    print(f'{contpares}')
+    contpares = contpares + 2
+
+print("IMPARES:")
+while contimpares <= 99:
+    print(f'{contimpares}')
+    contimpares = contimpares +2
+
